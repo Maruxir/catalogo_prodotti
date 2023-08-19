4d08097924e0c9f967c845f57b650ee241faff3f
Maruxir <irene.marucci00@gmail.com>
2023-08-19 12:50:11 +0200

    creazione progetto

---
+++ Catalogo_prodotti_spring/src/main/java/com/prenotazioni/demo/CatalogoProdottiSpringApplication.java
@@ -0,0 +1,13 @@
+package com.prenotazioni.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoProdottiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogoProdottiSpringApplication.class, args);
+	}
+
+}
